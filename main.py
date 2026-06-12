c976602b96ebeaa563cb9e0a5cfab935ea80906b
VardinsDev <vardins@vardinsdev.org>
2026-06-12 09:02:32 -0500

    Chore: Cleaned up loop structure Add: Update command logic Chore: created a loadData function

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import os
 def json_write(description, id, status, createdAt, updatedAt):
     filename = "data.json"
 
-    if os.path.exists(filename) and os.path.getsize(filename) > 0:
-        with open(filename, "r", encoding="utf-8") as read_file:
-            data = json.load(read_file)
-    else:
-        data=[]
+    data = loadData()
 
     new_data = {
         "id": id,
@@ -20,6 +16,30 @@ def json_write(description, id, status, createdAt, updatedAt):
     }
 
     data.append(new_data)
+
+    with open(filename, "w", encoding="utf-8") as write_file:
+        json.dump(data, write_file, indent=4)
+
+def loadData():
+    filename = "data.json"
+    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+        with open(filename, "r", encoding="utf-8") as read_file:
+            data = json.load(read_file)
+    else:
+        data = []
+
+    return data
+
+def updateData(id, description, updatedTime):
+    filename = "data.json"
+
+    data = loadData()
+
+    for task in data:
+        if task["id"] == id:
+            task["updatedAt"] = updatedTime
+            task["description"] = description
+            print("Change successful!")
 
     with open(filename, "w", encoding="utf-8") as write_file:
         json.dump(data, write_file, indent=4)
@@ -47,7 +67,9 @@ while (True):
         print("Type task-cli help for more commands")
         continue
     userInput = userInput.replace("task-cli ", "")
-    if "help" in userInput:
+    userInput = userInput.split()
+    if not userInput: continue
+    if "help" == userInput[0]:
         print("TaskCLI Help")
         print('add - Used to add a task, ex: task-cli add "Buy Groceries"')
         print('update - Used to update a task based on task id, ex: task-cli update 1 "Buy groceries and cook dinner')
@@ -58,11 +80,16 @@ while (True):
         print('list done - Used to list all finished tasks')
         print('list todo - Used to list all tasks that are not started yet')
         print('list in-progress - Used to list all in progress tasks')
-    if "add" in userInput:
-        userInput = userInput.replace("add ", "")
+    elif "add" == userInput[0]:
+        userInput.pop(0)
+        userInput = " ".join(userInput)
         taskId = id_chooser()
         json_write(userInput, taskId, "todo", time.ctime(), time.ctime())
-        print("Task added successfully (ID: " + str(taskId) + ")")
-    if "update" in userInput:
-        userInput = userInput.replace("update ", "")
+        print(f"Task added successfully (ID: {taskId})")
+    elif "update" == userInput[0]:
+        userInput.pop(0)
+        id = int(userInput[0])
+        userInput.pop(0)
+        userInput = " ".join(userInput)
+        updateData(id, userInput, time.ctime())
 
